611adc942f9e1e064f57fa57f05e53a096bc2959
ngabonzizacedrickkennedy <cedzlankee42@gmail.com>
2026-06-27 11:28:52 +0200

    ch

---
+++ Backend/Document-Portfolio-Service/src/test/java/com/ced/Document/Portfolio/Service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ced.Document.Portfolio.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Backend/Reporting-Analytics-Service/src/test/java/com/ced/Reporting/Analytics/Service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ced.Reporting.Analytics.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
